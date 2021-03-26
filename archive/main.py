13cabe7c43b0a04925fa14b8f617e6fa1071e36d
Anatoly Kopyl <akopyl@radner.ru>
2021-03-27 02:32:56 +0300

    :card_file_box: Генератор базы данных готов!

--- archive/main.py
+++ archive/main.py
@@ -1,8 +1,14 @@
 import os
 from bs4 import BeautifulSoup
-import pymongo
+from pymongo import MongoClient
+from decouple import config
 
 DIRECTORY = "messages/"
+HOST_NAME = config('HOST_NAME')
+HOST_URL = config('HOST_URL')
+
+client = MongoClient(host="localhost", port=27017)
+db = client.vk_bingo
 
 for filename in os.listdir(DIRECTORY):
   if filename.endswith(".html"):
@@ -12,6 +18,16 @@ for filename in os.listdir(DIRECTORY):
       if (str(attch_desc.string) == "Фотография"):
         image = str(attch_desc.find_next_sibling().string)
         head = attch_desc.find_next_sibling().parent.parent.parent.find_previous_sibling()
-        name = str(head.a and head.a.string or "Толя Копыл")
+        name = str(head.a and head.a.string or HOST_NAME)
         date = str(head.text).split(", ")[1].replace(" (ред.)", "")
+        link = str(head.a and head.a['href'] or HOST_URL)
+        
+        card = {
+          "image": image,
+          "name": name,
+          "link": link,
+          "date": date
+        }
+
+        db.cards.insert_one(card)
         
